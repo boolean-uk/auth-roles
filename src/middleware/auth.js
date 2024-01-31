e80943073f9e4e31c83df6875ba1db35ac2a86c7
Satoki <satokiito@gmail.com>
2024-01-31 16:15:10 +0000

    working on verifyAdmin function as tests not passing as expected

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,42 @@
 const jwt = require('jsonwebtoken')
 const secret = process.env.JWT_SECRET
+const { findUserDb } = require('../domains/user')
 
-const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization
+const verifyToken = async (req, res, next) => {
+    const token = req.headers.authorization.slice(7)
 
     if (!token) {
         return res.status(400).json({
             error: 'Missing authorisation token'
         })
     }
-    const splitToken = token.slice(7)
 
     try {
-        const verifiedToken = jwt.verify(splitToken, secret)
+        const verifiedToken = jwt.verify(token, secret)
         req.token = verifiedToken
+        const userID = verifiedToken.sub
+        const foundUser = await findUserDb(userID)
+        delete foundUser.passwordHash
+        req.user = foundUser
+
         next()
     } catch (err) {
-        return res.status(400).json({ error: "Invalid credentials" })
+        return res.status(400).json({ error: "Invalid or expired token" })
     }
 }
 
-const verifyAdmin = async (req, res, next) => {
-    const userID = req.token.sub
+const verifyAdmin = (req, res, next) => {
+    if (!req.user) {
+        return res.status(401).json({ message: 'Unauthorised' })
+    }
 
-    const findUser = await findUserDb()
+    if (req.user.role !== 'ADMIN') {
+        return res.status(403).json({
+            error: "You do not have permission to view all users."
+        })
+    }
+
+    next()
 }
 
 module.exports = { verifyToken, verifyAdmin }
