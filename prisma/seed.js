17c155ee42b38c0d0b9bc43bd27605793f3defc9
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-31 17:26:01 +0000

    feat: add role and permission models

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,17 +4,24 @@ const bcrypt = require("bcrypt");
 const { faker } = require("@faker-js/faker");
 
 async function seed() {
+  const userRole = await createRole("USER");
+  await createPermissions(userRole);
+
   const users = [];
 
   while (users.length < 10) {
-    const user = await createUser(faker.internet.userName(), "123456789");
+    const user = await createUser(
+      faker.internet.userName(),
+      "123456789",
+      userRole
+    );
     users.push(user);
   }
 
   process.exit(0);
 }
 
-async function createUser(username, password) {
+async function createUser(username, password, role) {
   const posts = [];
 
   for (let i = 0; i < username.length; i++) {
@@ -28,15 +35,60 @@ async function createUser(username, password) {
       posts: {
         create: posts,
       },
+      roles: {
+        create: {
+          roleId: role.id,
+        },
+      },
     },
     include: {
       posts: true,
+      roles: {
+        select: {
+          role: {
+            select: {
+              type: true,
+            },
+          },
+        },
+      },
     },
   });
 
   console.log("User created", user);
+  console.log(user.roles);
 
   return user;
+}
+
+async function createPermissions(role) {
+  /**
+   * @type {("CREATE" | "READ" | "UPDATE" | "DELETE")[]}
+   */
+  const operations = ["CREATE", "READ", "UPDATE", "DELETE"];
+
+  operations.forEach((operation) => {
+    prisma.permission.create({
+      data: {
+        operation,
+        target: "OWN",
+        resource: "POST",
+        role: {
+          create: {
+            roleId: role.id,
+          },
+        },
+      },
+    });
+  });
+}
+
+async function createRole(type) {
+  return await prisma.role.create({
+    data: {
+      type: type,
+    },
+  });
 }
 
 seed()
