b3db5f98534d55070f926299e8ed071e51dff510
Lukas Dembicki <lad_777@hotmail.com>
2024-01-31 17:56:15 +0000

    BUILD: get router.

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,5 +6,6 @@ const {
 const router = express.Router();
 
 router.post("/", createUser);
+router.get("/", (req, res) => {console.log("Hello I am a console log", req.headers.authorization)})
 
 module.exports = router;
