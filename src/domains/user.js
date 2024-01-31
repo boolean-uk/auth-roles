4477b384ce3611e9646b77b358edb67181a349bd
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-31 17:33:54 +0000

    feat: createUser assigns user role

--- src/domains/user.js
+++ src/domains/user.js
@@ -1,12 +1,39 @@
 const prisma = require('../utils/prisma')
 const bcrypt = require('bcrypt')
 
-const createUserDb = async (username, password) => await prisma.user.create({
-  data: {
-    username,
-    passwordHash: await bcrypt.hash(password, 6)
-  }
-})
+const createUserDb = async (username, password) => {
+  return await prisma.user.create({
+    data: {
+      username,
+      passwordHash: await bcrypt.hash(password, 6),
+      roles: {
+        create: {
+          role: {
+            connectOrCreate: {
+              create: {
+                type: "USER"
+              },
+              where: {
+                type: "USER" 
+              }
+            }
+          }
+        }
+      }
+    },
+    include: {
+      roles: {
+        include: {
+          role: {
+            select: {
+              type: true
+            }
+          }
+        }
+      }
+    }
+  });
+}
 
 module.exports = {
   createUserDb
