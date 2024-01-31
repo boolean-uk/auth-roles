700ad2189bebb1817e61b5bd9d360cc53ec3586b
Lukas Dembicki <lad_777@hotmail.com>
2024-01-31 17:56:04 +0000

    FORMAT: Blanket formatting.

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,33 +1,34 @@
-const { PrismaClientKnownRequestError } = require("@prisma/client")
-const { createPostDb } = require('../domains/post.js')
+const { PrismaClientKnownRequestError } = require("@prisma/client");
+const { createPostDb } = require("../domains/post.js");
 
 const createPost = async (req, res) => {
-  const {
-    title,
-    userId
-  } = req.body
+    const { title, userId } = req.body;
 
-  if (!title || !userId) {
-    return res.status(400).json({
-      error: "Missing fields in request body"
-    })
-  }
-
-  try {
-    const createdPost = await createPostDb(title, userId)
-
-    return res.status(201).json({ post: createdPost })
-  } catch (e) {
-    if (e instanceof PrismaClientKnownRequestError) {
-      if (e.code === "P2025") {
-        return res.status(409).json({ error: "A user with the provided ID does not exist" })
-      }
+    if (!title || !userId) {
+        return res.status(400).json({
+            error: "Missing fields in request body",
+        });
     }
 
-    res.status(500).json({ error: e.message })
-  }
-}
+    try {
+        const createdPost = await createPostDb(title, userId);
+
+        return res.status(201).json({ post: createdPost });
+    } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+            if (e.code === "P2025") {
+                return res
+                    .status(409)
+                    .json({
+                        error: "A user with the provided ID does not exist",
+                    });
+            }
+        }
+
+        res.status(500).json({ error: e.message });
+    }
+};
 
 module.exports = {
-  createPost
-}
+    createPost,
+};
