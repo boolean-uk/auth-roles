8c7e06670fe0d272beea20df89470f5e2a4b5ade
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-31 17:38:36 +0000

    test: amend helper createUser

--- test/helpers/createUser.js
+++ test/helpers/createUser.js
@@ -1,12 +1,31 @@
 const prisma = require("../../src/utils/prisma")
 const bcrypt = require('bcrypt');
 
+/**
+ * @param {string} username 
+ * @param {string} password 
+ * @param {"USER" | "ADMIN"} role 
+ * @returns 
+ */
 const createUser = async (username, password, role = 'USER') => {
   return await prisma.user.create({
     data: {
       username,
-      role,
-      passwordHash: await bcrypt.hash(password, 6)
+      passwordHash: await bcrypt.hash(password, 6),
+      roles: {
+        create: {
+          role: {
+            connectOrCreate: {
+              create: {
+                type: role
+              },
+              where: {
+                type: role
+              }
+            }
+          }
+        }
+      }
     },
   })
 }
