44ea7fd5e058f90aebef960f880ce8932b51c079
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-31 18:21:44 +0000

    feat: change get /users to admin only

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,10 +2,11 @@ const express = require("express");
 const {
   createUser, getAllUsers
 } = require('../controllers/user');
+const { authenticate, adminAuthorize } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/", createUser);
-router.get("/", getAllUsers);
+router.get("/", authenticate, adminAuthorize, getAllUsers);
 
 module.exports = router;
