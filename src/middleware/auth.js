0cc4cb71dc76dcc4c15c2d14354041c6045f70d4
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-01 12:56:35 +0000

    feat: update authentication to detect owners

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,45 @@
 const jwt = require("jsonwebtoken")
 const { getUserByIdDb } = require("../domains/user")
 
-const verifyAdminRole = async (req, res, next) => {
-    const header = req.headers.authorization
+
+const decodeToken = (header) => {
     if (!header) {
         throw new Error("unauthorised")
     }
     const token = header.slice(7, header.length)
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+    return decodedToken
+}
+
+const verifyAdminRole = async (req, res, next) => {
+    const header = req.headers.authorization
+    const decodedToken = decodeToken(header)
     const foundUser = await getUserByIdDb(decodedToken.sub)
     if (!foundUser || foundUser.role !== "ADMIN") {
+        if(req.params.id) {
+            return next()    
+        }
+        throw new Error("forbidden")
+    }
+    req.headers.verifiedAdmin = true
+    next()
+}
+
+const verifyOwner = async (req, res, next) => {
+    const verifiedAdmin = req.headers.verifiedAdmin
+    if (verifiedAdmin) {
+        return next()
+    } 
+    const header = req.headers.authorization
+    const decodedToken = decodeToken(header)
+
+    if (decodedToken.sub !==  Number(req.params.id)) {
         throw new Error("forbidden")
     }
     next()
 }
 
-module.exports = { verifyAdminRole }
+module.exports = { 
+    verifyAdminRole, 
+    verifyOwner
+ }
