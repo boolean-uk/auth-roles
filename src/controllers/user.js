8a27fae5f6ecbe17928aae7a1dd6c3984c0b24ef
Lukas Dembicki <lad_777@hotmail.com>
2024-02-01 15:24:10 +0000

    FEAT: createUser now asigns tokens and passes user id through.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,17 +1,20 @@
-const { PrismaClientKnownRequestError } = require("@prisma/client");
 const { createUserDb, getUsersDb } = require("../domains/user.js");
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET
 
 const createUser = async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, role } = req.body;
 
     if (!username || !password) {
         return res.status(400).json({
             error: "Missing fields in request body",
         });
     }
-        const createdUser = await createUserDb(username, password);
+        const createdUser = await createUserDb(username, password, role);
 
-        return res.status(201).json({ user: createdUser });
+        const token = jwt.sign(createdUser.id, secret)
+
+        return res.status(201).json({ user: createdUser, token: token });
     
 };
 
