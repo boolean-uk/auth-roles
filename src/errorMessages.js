e68cbaa46b5977d0cdfde9f6784059b0b6620dfc
Satoki <satokiito@gmail.com>
2024-02-01 15:00:20 +0000

    add errorMessages.js

---
+++ src/errorMessages.js
@@ -0,0 +1,6 @@
+const errorMessages = {
+    missingToken: "Missing authorisation token",
+    unauthorisedUser: "Unauthorised user",
+    insufficientPermissons: "You do not have sufficient permission.",
+    noPost: "Post does not exist"
+}
