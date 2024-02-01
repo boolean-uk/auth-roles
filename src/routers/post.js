203230fb2aabcd8fa44104db39ca8c08fab3c330
Satoki <satokiito@gmail.com>
2024-02-01 14:40:55 +0000

    add verifyPostDeletePermissons middleware to delete posts route

--- src/routers/post.js
+++ src/routers/post.js
@@ -3,12 +3,12 @@ const {
   createPost,
   deletePosts
 } = require('../controllers/post');
-const { verifyToken, verifyDeletePermissions } = require("../middleware/auth");
+const { verifyToken, verifyPostDeletePermissions } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/", createPost);
 
-router.delete("/:postId", verifyToken, verifyDeletePermissions, deletePosts)
+router.delete("/:postId", verifyToken, verifyPostDeletePermissions, deletePosts)
 
 module.exports = router;
