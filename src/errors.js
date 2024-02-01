65f2fb5b2f158dc45477e0f5d4278989b2332253
Lukas Dembicki <lad_777@hotmail.com>
2024-02-01 12:30:19 +0000

    BUILD: error file for compiling new error messages

---
+++ src/errors.js
@@ -0,0 +1,8 @@
+
+// Just trying to understand the purpose of doing this. For future error handling and mapping through to send client proper error message. Not implemented. 
+
+const errorMessages = {
+    "Cannot convert undefined or null to object": 'Invalid Credentials'
+}
+
+module.exports = errorMessages
