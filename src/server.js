7f0aa9cf35b1f5617dd09a71fcb59288c4708442
Lukas Dembicki <lad_777@hotmail.com>
2024-02-01 10:18:50 +0000

    BUILD: added express async error handling.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
 
 const cors = require('cors');
@@ -16,5 +17,11 @@ app.use('/users', userRouter);
 
 const postRouter = require('./routers/post');
 app.use('/posts', postRouter);
+
+app.use((err, req, res, next)=> {
+    console.log(err)
+
+    res.status(500).json({message: "Internal error"})
+})
 
 module.exports = app
