3fb890a385d8972399f622b0fbead576565dc4a1
Satoki <satokiito@gmail.com>
2024-02-01 14:39:12 +0000

    complete verifyPostDeletePermissons func

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const secret = process.env.JWT_SECRET
 const { findUserDb } = require('../domains/user')
+const { findPostDb } = require('../domains/post')
 
 const verifyToken = async (req, res, next) => {
     const token = req.headers.authorization.slice(7)
@@ -55,6 +56,8 @@ const verifyDeletePermissions = async (req, res, next) => {
 
 const verifyPostDeletePermissions = async (req, res, next) => {
     const postId = Number(req.params.postId)
+    const postToDelete = await findPostDb(postId)
+
     const currentUserId = req.token.sub
     const currentUser = await findUserDb(currentUserId)
 
@@ -62,14 +65,17 @@ const verifyPostDeletePermissions = async (req, res, next) => {
         return res.status(401).json({ message: 'Unauthorised user' })
     }
 
-    if (currentUser.role !== 'ADMIN' && currentUser.id !== userToDeleteID) {
+    if (!postToDelete) {
+        return res.status(400).json({ message: "Post does not exist" })
+    }
+
+    if (currentUser.role !== 'ADMIN' && currentUser.id !== postToDelete.userId) {
         return res.status(403).json({
             error: "You do not have sufficient permission."
         })
     }
 
-    delete currentUser.passwordHash
     next()
 }
 
-module.exports = { verifyToken, verifyAdmin, verifyDeletePermissions }
+module.exports = { verifyToken, verifyAdmin, verifyDeletePermissions, verifyPostDeletePermissions }
