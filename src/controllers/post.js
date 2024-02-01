ac15f1efcfb0e4809255957dacb3c19b98bf0a24
Lukas Dembicki <lad_777@hotmail.com>
2024-02-01 17:50:30 +0000

    BUILD: deletePost controller

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,34 +1,29 @@
-const { PrismaClientKnownRequestError } = require("@prisma/client");
-const { createPostDb } = require("../domains/post.js");
+const { createPostDb, deletePostDb } = require("../domains/post.js");
 
 const createPost = async (req, res) => {
-    const { title, userId } = req.body;
+    const {title}  = req.body;
+    const userId = req.user.id
+
 
     if (!title || !userId) {
         return res.status(400).json({
             error: "Missing fields in request body",
         });
     }
-
-    try {
         const createdPost = await createPostDb(title, userId);
 
         return res.status(201).json({ post: createdPost });
-    } catch (e) {
-        if (e instanceof PrismaClientKnownRequestError) {
-            if (e.code === "P2025") {
-                return res
-                    .status(409)
-                    .json({
-                        error: "A user with the provided ID does not exist",
-                    });
-            }
-        }
-
-        res.status(500).json({ error: e.message });
-    }
 };
+
+const deletePost = async (req, res) => {
+    const postId  = Number(req.params.id)
+    
+    const deletedPost = await deletePostDb(postId)
+    return res.status(200).json({ post: deletedPost })
+}
+
 
 module.exports = {
     createPost,
+    deletePost
 };
