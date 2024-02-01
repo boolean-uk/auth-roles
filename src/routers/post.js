1d551b4e91ae48bc55954a20a3661b7132b0de90
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-02-01 16:00:17 +0000

    feat: implement auth check for delete /post/:id

--- src/routers/post.js
+++ src/routers/post.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { createPost, deletePost } = require("../controllers/post");
+const { checkUserPermission, authenticate } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/", createPost);
-router.delete("/:id", deletePost)
+router.delete(
+  "/:id",
+  authenticate,
+  checkUserPermission("DELETE", "POST"),
+  deletePost
+);
 
 module.exports = router;
