4ba1727f247c3a722999e1ee91fd62cd72bc4cf4
Kye <NovusAa12@proton.me>
2024-02-03 13:22:01 +0000

    update routers/user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const {
-  createUser
-} = require('../controllers/user');
+const { createUser, getAllUsers } = require("../controllers/user");
+const { verifyAdminRole, verifyToken } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/", createUser);
+
+router.get("/", verifyToken, verifyAdminRole, getAllUsers);
 
 module.exports = router;
