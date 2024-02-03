90f04f7a0a0e4969fa4852313abd885fc9363ff7
Kye <NovusAa12@proton.me>
2024-02-03 13:21:51 +0000

    update controllers/user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,33 +1,39 @@
-const { PrismaClientKnownRequestError } = require("@prisma/client")
-const { createUserDb } = require('../domains/user.js')
+const { PrismaClientKnownRequestError } = require("@prisma/client");
+const { createUserDb, getAllUsersDb } = require("../domains/user.js");
 
 const createUser = async (req, res) => {
-  const {
-    username,
-    password
-  } = req.body
+  const { username, password, role } = req.body;
 
   if (!username || !password) {
-    return res.status(400).json({
-      error: "Missing fields in request body"
-    })
+    return res.status(400).send({
+      error: "Missing fields in request body",
+    });
   }
 
   try {
-    const createdUser = await createUserDb(username, password)
+    const createdUser = await createUserDb(username, password, role);
 
-    return res.status(201).json({ user: createdUser })
+    return res.status(201).send({ user: createdUser });
   } catch (e) {
     if (e instanceof PrismaClientKnownRequestError) {
       if (e.code === "P2002") {
-        return res.status(409).json({ error: "A user with the provided username already exists" })
+        return res
+          .status(409)
+          .send({ error: "A user with the provided username already exists" });
       }
     }
 
-    res.status(500).json({ error: e.message })
+    res.status(500).send({ error: e.message });
   }
-}
+};
+
+const getAllUsers = async (req, res) => {
+  const allUsers = await getAllUsersDb();
+
+  return res.status(200).send({ users: allUsers });
+};
 
 module.exports = {
-  createUser
-}
+  createUser,
+  getAllUsers,
+};
