8efa1c61cd2cfb52dc5f17f6c4653dd1405438cc
Kye <NovusAa12@proton.me>
2024-02-03 13:16:07 +0000

    update domain/users.js

--- src/domains/user.js
+++ src/domains/user.js
@@ -1,13 +1,24 @@
-const prisma = require('../utils/prisma')
-const bcrypt = require('bcrypt')
+const prisma = require("../utils/prisma");
+const bcrypt = require("bcrypt");
 
-const createUserDb = async (username, password) => await prisma.user.create({
-  data: {
-    username,
-    passwordHash: await bcrypt.hash(password, 6)
-  }
-})
+const createUserDb = async (username, password, role) =>
+  await prisma.user.create({
+    data: {
+      username,
+      passwordHash: await bcrypt.hash(password, 6),
+      role,
+    },
+  });
+
+const getAllUsersDb = async () => await prisma.user.findMany();
+
+const findUserByUsernameDb = async (username) =>
+  await prisma.user.findUnique({
+    where: { username },
+  });
 
 module.exports = {
-  createUserDb
-}
+  createUserDb,
+  findUserByUsernameDb,
+  getAllUsersDb,
+};
