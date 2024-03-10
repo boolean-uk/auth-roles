b86028d63a7a1fde1418f52652e962728ca0d053
Mobeen <muhammadmobeen1@hotmail.com>
2024-03-10 15:33:56 +0100

    done

--- src/domains/user.js
+++ src/domains/user.js
@@ -1,13 +1,30 @@
-const prisma = require('../utils/prisma')
-const bcrypt = require('bcrypt')
+const prisma = require("../utils/prisma");
+const bcrypt = require("bcrypt");
 
-const createUserDb = async (username, password) => await prisma.user.create({
-  data: {
-    username,
-    passwordHash: await bcrypt.hash(password, 6)
-  }
-})
+const createUserDb = async (username, password, role) =>
+  await prisma.user.create({
+    data: {
+      username,
+      passwordHash: await bcrypt.hash(password, 6),
+      role,
+    },
+  });
+
+const getAllUsersDb = async () => await prisma.user.findMany();
+
+const deleteUserByIdDb = async (id) =>
+  await prisma.user.delete({
+    where: { id },
+  });
+
+const findUserByIdDb = async (id) =>
+  await prisma.user.findUnique({
+    where: { id },
+  });
 
 module.exports = {
-  createUserDb
-}
+  createUserDb,
+  getAllUsersDb,
+  deleteUserByIdDb,
+  findUserByIdDb,
+};
---
+++ src/errors/errors.js
@@ -0,0 +1,7 @@
+const errors = {
+  missingFields: "Missing required fields",
+  forbidden: "Forbidden",
+  unauthorized: "Unauthorized",
+};
+
+module.exports = errors;
